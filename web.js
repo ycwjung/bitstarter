8c69b8a5f2caaa141310ce13a53fed748b5c12ab
Wayne Jung <ycwjung@yahoo.com>
2013-07-02 19:34:47 +0000

    web.js committed 07/02/2013

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Here we come! And again. And again.');
+  response.send('Hello World! Here we come! And again. And again. OMG! Not again!!!');
 });
 
 var port = process.env.PORT || 5000;
