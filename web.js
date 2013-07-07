c818e00378e0bed1af5d9c004340c034d35763b7
Wayne Jung <ycwjung@yahoo.com>
2013-07-07 01:31:30 +0000

    One more try!

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Here we come! And again. And again. OMG! Not again!!! Before starting the crowdfunded site!!!');
+  response.send('Hello World! Here we come! \n 
+  And again. And again. OMG! Not again!!! \n
+  Before starting the crowdfunded site!!!');
 });
 
 var port = process.env.PORT || 5000;
