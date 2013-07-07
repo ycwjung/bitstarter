266580f8c59b863546bcfd1ab396959415133cba
Wayne Jung <ycwjung@yahoo.com>
2013-07-07 02:45:07 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Here we come! \n And again. And again. OMG! Not again!!! \n Before starting the crowdfunded site!!! Looks like it is not that hard after all.');
+  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
